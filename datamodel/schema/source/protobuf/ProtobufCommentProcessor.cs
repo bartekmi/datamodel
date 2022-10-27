6e176f0bd1d5244eba2103383034c1478d340ddf
bartekmi <220bartek@gmail.com>
2022-10-26 18:32:58 -0600

    Start of formatter for comments for protobuf

--- datamodel/schema/source/protobuf/ProtobufCommentProcessor.cs
+++ datamodel/schema/source/protobuf/ProtobufCommentProcessor.cs
@@ -1,5 +1,32 @@
+using System;
+using System.IO;
+using System.Text;
+
 namespace datamodel.schema.source.protobuf {
     public static class ProtobufCommentProcessor {
-        
+        public static string ProcessComments(string raw) {
+            string line = null;
+            bool startNewParagraph = true;
+            StringBuilder builder = new StringBuilder();
+
+            using (TextReader reader = new StringReader(raw)) {
+                while ((line = reader.ReadLine()) != null) {
+                    if (string.IsNullOrWhiteSpace(line)) {
+                        if (builder.Length == 0)        // No leading empty lines
+                            continue;
+                        builder.AppendLine();
+                        startNewParagraph = true;
+                    } else {
+                        if (startNewParagraph)
+                            startNewParagraph = false;
+                        else
+                            builder.Append(' ');
+                        builder.Append(line.Trim());
+                    }
+                }
+            }
+
+            return builder.ToString().TrimEnd();
+        }
     }
 }
