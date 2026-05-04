2e33ae958eb5d2834daa8b7239d3619369beb2a8
Muszynski <bartek.muszynski@gehealthcare.com>
2026-05-04 09:23:14 -0700

    Dump schema when generating

--- ge_extract/ge-model.sh
+++ ge_extract/ge-model.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
+set -x
 
 # git-pull the two source packages to ensure latest code snapshot
-git -C ~/gitlab/docs-site pull --ff-only
 git -C ~/gitlab/dpe-date-domain pull --ff-only
 
 # Build the Java extractor code and use it to parse three dirs: model, model/locationmaster and entity.
@@ -13,7 +13,7 @@ java -jar target/javaparser-minimal-1.0.0.jar ~/gitlab/dpe-date-domain/src/main/
 java -jar target/javaparser-minimal-1.0.0.jar ~/gitlab/dpe-date-domain/src/main/java/com/gehealthcare/dpsa/entity /tmp/datamodel/java_entities.yaml
 
 # Run the C# Data Model Visualizer
-dotnet run --project ../datamodel -- ge-java
+dotnet run --project ../datamodel -- ge-java dumpSchema=/tmp/datamodel/schema.json
 
 # Zip-up the generated output
 pushd /tmp
